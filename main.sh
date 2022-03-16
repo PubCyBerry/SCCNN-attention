12ec5a0ae491e5b085654f7b244e908121373291
PubCyBerry <taehunkim@hanyang.ac.kr>
2022-03-16 15:55:07 +0000

    edit main.sh for new config

--- main.sh
+++ main.sh
@@ -9,11 +9,6 @@ dir_path=$(dirname "${BASH_SOURCE[0]}")
 # bash main.sh src/data/loader.py == python "${dir_path}/src/data/loader.py"
 
 for i in $(seq 0 20); do
-    # python run.py log.project_name='SCCNN_LSTM' optimizer.lr=1e-4 log.device.gpu=0 data.roi=$i
-    python run.py log.project_name='SCCNN_LSTM_roi_rank' optimizer.lr=1e-4 log.device.gpu=0 data.roi=$i
+    python run.py log.project_name='SCCNN_LSTM_roi_rank' log.model_name='SCCNN_LSTM_roi_rank' optimizer.lr=1e-4 log.device.gpu=0 data.roi=$i
 done
 
-# for i in $(seq 0 20); do
-#     # python run.py log.project_name='SCCNN_LSTM' optimizer.lr=1e-4 log.device.gpu=0 data.roi=$i
-#     python run.py log.project_name='SCCNN_LSTM_roi_rank' optimizer.lr=1e-3 log.device.gpu=0 data.roi=$i
-# done
