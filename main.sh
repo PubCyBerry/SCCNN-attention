b83389412d1fc94edb0d30aa07a8882122a8667f
PubCyBerry <taehunkim@hanyang.ac.kr>
2022-03-16 16:00:11 +0000

    parameterize project name of main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+# set -ex
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 dir_path=$(dirname "${BASH_SOURCE[0]}")
 
@@ -9,6 +9,6 @@ dir_path=$(dirname "${BASH_SOURCE[0]}")
 # bash main.sh src/data/loader.py == python "${dir_path}/src/data/loader.py"
 
 for i in $(seq 0 20); do
-    python run.py log.project_name='SCCNN_LSTM_roi_rank' log.model_name='SCCNN_LSTM_roi_rank' optimizer.lr=1e-4 log.device.gpu=0 data.roi=$i
+    python run.py log.project_name='${1:-SCCNN_LSTM_roi_rank}' log.model_name='SCCNN_LSTM_roi_rank' optimizer.lr=1e-4 log.device.gpu=0 data.roi=$i
 done
 
